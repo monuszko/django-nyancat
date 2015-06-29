263cf5b6df800e76381e31ced863df5d6224f30b
Marek Onuszko <marek.onuszko@gmail.com>
2015-06-29 13:47:01 +0200

    cache most_popular

--- nyancat/urls.py
+++ nyancat/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
+#TODO: probably better to put stuff like ONE_HOUR in constants.py or something
 
 from . import views
 from .forms import VideoForm 
@@ -14,7 +16,8 @@ urlpatterns = (
         url(r'^remove_videos/$',
             views.remove_videos, name='remove_videos'),
         url(r'^most_popular/$',
-            views.MostPopular.as_view(), name='most_popular'),
+            cache_page(views.ONE_HOUR)(views.MostPopular.as_view()),
+            name='most_popular'),
         url(r'^restore_cookie/(?P<person_url>[\w.@+-]+)/(?P<token>[\w.:_=-]+)$',
             views.restore_cookie, name='restore_cookie'),
         url(r'^send_password/(?P<person_url>[a-zA-Z0-9]+)/$',
